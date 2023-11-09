6bb816ba55ddcfa4d1b04976d815ed7e5c07fbd8
LOVEKOREAN <JIWON0521@SOOKMYUNG.AC.KR>
2023-11-09 15:35:17 +0900

    04

--- main.c
+++ main.c
@@ -4,10 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-char src[]="the worst things to eat before you sleep";
-char dst[100];
-strcpy(dst,src);
-printf("copied string : %s\n", dst);
+char str[30]="happy C programming";
+printf("문자열 \"%s\"의 길이 : %i\n", str, strlen(str));
   system("PAUSE");	
   return 0;
 }
